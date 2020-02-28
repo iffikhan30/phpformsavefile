93dfbb588f9a247344d0b6e8e6968881fa3dc3e5
iffikhan30 <irfan.sarwar.khan30@gmail.com>
2020-02-28 10:44:28 +0500

    initial

---
+++ form.php
@@ -0,0 +1,58 @@
+<?php
+
+if (!file_exists(date("Y"))) {
+    mkdir(date("Y"), 0777, true);
+    chmod(date("Y"), 0777);
+}
+if (!file_exists(date("Y") . '/' . date("m"))) {
+    mkdir(date("Y") . '/' . date("m"), 0777, true);
+    chmod(date("Y") . '/' . date("m"), 0777);
+}
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+if (isset($_POST['name'])) {
+    $fname = $_POST['name'];
+    $email = $_POST['email'];
+    $telephone = $_POST['phone'];
+    $comments = $_POST['message'];
+
+    $email_to = "irfan.sarwar.khan30@gmail.com";
+    $email_subject = "Contact Us form submitted";
+
+    $email_message = "Form details below.\n\n";
+
+    function clean_string($string)
+    {
+        $bad = array("content-type", "bcc:", "to:", "cc:", "href");
+        return str_replace($bad, "", $string);
+    }
+
+    $email_message .= "First Name: " . clean_string($fname) . "\n";
+    $email_message .= "Email: " . clean_string($email) . "\n";
+    $email_message .= "Telephone: " . clean_string($telephone) . "\n";
+    $email_message .= "Comments: " . clean_string($comments) . "\n";
+
+    if (!file_exists(date("Y") . '/' . date("m") . '/contactform')) {
+        mkdir(date("Y") . '/' . date("m") . '/contactform', 0777, true);
+        chmod(date("Y") . '/' . date("m") . '/contactform', 0777);
+    }
+
+    $content = $email_message;
+    $fp = fopen(date("Y") . '/' . date("m") . '/contactform/' . date('d-h-i-s') . "-form.txt", "wb");
+    fwrite($fp, $content);
+    fclose($fp);
+
+// create email headers
+    $headers = 'From: ' . $email . "\r\n" .
+        'Reply-To: ' . $email . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+
+    if (mail($email_to, $email_subject, $email_message, $headers)) {
+        echo '1';
+    } else {
+        echo '0';
+    }
+}
+?>
---
+++ index.php
@@ -0,0 +1,98 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+</head>
+<body>
+<section class="form">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+
+                <form name="contactform" id="contactform" class="AnimationForm">
+                    <div class="form-group">
+                        <label for="name">Name</label>
+                        <input type="text" class="form-control" id="name" name="name" placeholder="Enter Name">
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email address</label>
+                        <input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelp"
+                               placeholder="Enter email">
+                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone
+                            else.</small>
+                    </div>
+                    <div class="form-group">
+                        <label for="phone">Phone</label>
+                        <input type="tel" class="form-control" name="phone" id="phone" placeholder="Enter Phone">
+                    </div>
+                    <div class="form-group">
+                        <label for="message">Message</label>
+                        <textarea name="message" class="form-control">Tell us about your project</textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="jquery.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+        crossorigin="anonymous"></script>
+<script src="jquery.validate.js"></script>
+<script>
+    $(document).ready(function () {
+
+        $("#contactform").validate({
+            ignore: ":hidden",
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 3
+                },
+                email: {
+                    required: true,
+                    minlength: 3
+                },
+                phone: {
+                    required: true
+                },
+                message: {
+                    required: true,
+                    minlength: 10
+                }
+            },
+            submitHandler: function (form) {
+                console.log('valid form submission'); // for demo
+                $.ajax({
+                    type: "POST",
+                    url: "form.php",
+                    data: $(form).serialize(),
+                    success: function () {
+                        $(form).html("<div id='message'></div>");
+                        $('#message').html("<h2>We’re thrilled to hear from you. One of our awesome consultants will get in touch with you soon.<br/>All the best,<br/></h2>");
+                    }
+                });
+                return false; // required to block normal submit since you used ajax
+            }
+
+        });
+
+    });
+</script>
+</body>
+</html>
